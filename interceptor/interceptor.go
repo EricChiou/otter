9d4b6bf3a8f9bf96a4d02299635aa8b167c38b1e
Chiu <Eric.Chiu@acereb.com>
2019-12-16 09:43:02 +0800

    fix interceptor

--- interceptor/interceptor.go
+++ interceptor/interceptor.go
@@ -9,7 +9,12 @@ import (
 
 // Interceptor check jwt
 func Interceptor(ctx *fasthttp.RequestCtx) (jwt.Payload, bool) {
+	var payload jwt.Payload
 	auth := string(ctx.Request.Header.Peek("Authorization"))
+	if len(auth) < len(cons.Setting.TokenPrefix) {
+		return payload, false
+	}
+
 	payload, result := jwt.Verify(auth[len(cons.Setting.TokenPrefix):])
 	if !result {
 		return payload, false
