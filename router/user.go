fab0a108eb218ed7a28dfb301a4b146317ab50ac
EricChiou <miatlab@gmail.com>
2020-12-18 17:18:41 +0800

    change sign in api method from GET to POST

--- router/user.go
+++ router/user.go
@@ -9,14 +9,11 @@ func initUserAPI() {
 	groupName := "/user"
 	var controller user.Controller
 
-	// Get
-	// sign in
-	get(groupName+"/signIn", false, nil, controller.SignIn)
-
 	// user list
 	get(groupName+"/list", true, nil, controller.List)
 
 	// Post
+	post(groupName+"/signIn", false, nil, controller.SignIn)
 	post(groupName, true, nil, controller.Update)
 	post(groupName+"/:userID", true, []acl.Code{acl.UpdateUser}, controller.UpdateByUserID)
 
